fa19599423753079dda3d25c05aac7f17a1a18d9
Christopher J. Braccia II <aiccarb@gmail.com>
2019-06-23 04:23:28 +0700

    Add files via upload

---
+++ MUD.py
@@ -0,0 +1,28 @@
+#sample mud - built from the tools used in Chapter 1 of "Automate the boring stuff".
+
+print('You awaken in a large and dim room.')
+print('You have no memory of this place.')
+print('A voice from a dream keeps calling out to you.')
+print('Greetings, I am Balthore - Guardian of this realm')
+print('What name do you go by?') #ask for their name
+myName = input()
+print('The hour of our meeting is late ' + myName + ' for we have so little light left.' )
+print('There is an evil in this wind, I can taste it')
+print('From which Isle of Man do you hail?') #ask for their nation
+myNation = input()
+print('A wise man once said "Only Steers and Queers come from the ' + myNation")
+print('What is your trade?')
+myTrade = input()
+print('Your childhood self would be dissapointed in your future selfes choices.')
+print('Which of the gods do you worship?')
+myGod = input ()
+print('I had not thought to find a follower of ' + myGod + ' in this realm..')
+print('You must depart this place at once, before the others arrive.')
+print('Will you take the Ship or will you go by Foot?')
+input ()
+
+
+
+
+
+
